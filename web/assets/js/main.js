d08fa8f9fd3822baaf9ac862a387084ffea599ab
BlackScorp <witalimik@web.de>
2014-08-31 16:19:17 +0200

    added controlls

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,97 +1,106 @@
-$(function(){
+$(function () {
 
-    // CHANGE THE INITIAL SEED HERE
-    Math.seed = 1337;
+    var controls, scene, camera, renderer, stats;
 
-    /**
-     * Math.seededRandom()
-     *
-     */
-    Math.seededRandom = function(max, min) {
-        max = max || 1;
-        min = min || 0;
+    function init() {
+        stats = new Stats();
+        stats.setMode(0);
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+        stats.domElement.style.zIndex = 2;
+        document.body.appendChild(stats.domElement);
 
-        Math.seed = (Math.seed * 9301 + 49297) % 233280;
-        var rnd = Math.seed / 233280.0;
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.z = 1000;
 
-        return min + rnd * (max - min);
-    }
+        renderer = new THREE.WebGLRenderer();
 
+        renderer.setSize(window.innerWidth, window.innerHeight);
 
-    var stats = new Stats();
-    stats.setMode(0);
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0px';
-    stats.domElement.style.top = '0px';
-    stats.domElement.style.zIndex = 2;
-    document.body.appendChild( stats.domElement );
+        renderer.domElement.id = 'map';
+        renderer.domElement.style.zIndex = 0;
+        renderer.domElement.style.top = 0;
+        renderer.domElement.style.left = 0;
+        renderer.domElement.style.position = 'absolute';
 
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.domElement.id = 'map';
-    renderer.domElement.style.zIndex = 0;
-    renderer.domElement.style.top = 0;
-    renderer.domElement.style.left = 0;
-    renderer.domElement.style.position = 'absolute';
-    document.body.appendChild( renderer.domElement );
+        document.body.appendChild(renderer.domElement);
 
+        controls = new THREE.TrackballControls(camera);
 
-    var geometry = new THREE.Geometry();
+        controls.rotateSpeed = 1.0;
+        controls.zoomSpeed = 1.2;
+        controls.panSpeed = 0.8;
 
+        controls.noZoom = false;
+        controls.noPan = false;
 
-    var material = new THREE.PointCloudMaterial( {
-        size: 80,
-        map: THREE.ImageUtils.loadTexture(
-            "assets/img/galactictop.png"
-        ),
-        blending: THREE.AdditiveBlending,
-        transparent: true
-    });
+        controls.staticMoving = true;
+        controls.dynamicDampingFactor = 0.3;
 
-    for(var galaxyIndex in galaxies){
-        var galaxy = galaxies[galaxyIndex];
-        var vector = new THREE.Vector3();
-        vector.y = galaxy.posY;
-        vector.z = galaxy.posZ;
-        vector.x = galaxy.posX;
+        controls.keys = [ 65, 83, 68 ];
 
-        geometry.vertices.push(vector);
+        controls.addEventListener('change', render);
 
     }
 
-    var sphere = new THREE.PointCloud( geometry, material );
-    sphere.sortParticles = true;
-    scene.add( sphere );
+    function createScene() {
+        var geometry = new THREE.Geometry();
 
-    camera.position.z = 1000;
-    $(window).on('mousewheel DOMMouseScroll',function(e){
-        var delta = e.originalEvent.detail || e.originalEvent.wheelDelta;
-        if(delta < 0){
-            camera.position.z -= 20;
-        }else{
-            camera.position.z += 20;
+
+        var material = new THREE.PointCloudMaterial({
+            size: 80,
+            map: THREE.ImageUtils.loadTexture(
+                "assets/img/galactictop.png"
+            ),
+            blending: THREE.AdditiveBlending,
+            transparent: true
+        });
+
+        for (var galaxyIndex in galaxies) {
+            var galaxy = galaxies[galaxyIndex];
+            var vector = new THREE.Vector3();
+            vector.y = galaxy.posY;
+            vector.z = galaxy.posZ;
+            vector.x = galaxy.posX;
+
+            geometry.vertices.push(vector);
+
         }
-    }).on('mousemove',function(e){
-        var camVector = new THREE.Vector3(e.pageX, e.pageY,0);
-        camera.lookAt(camVector);
 
-    }).on('resize',function(e){
+        var sphere = new THREE.PointCloud(geometry, material);
+        sphere.sortParticles = true;
+        scene.add(sphere);
+        render();
+    }
 
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        camera.aspect = (window.innerWidth / window.innerHeight);
-    });
+    function animate() {
 
+        requestAnimationFrame(animate);
+        controls.update();
+        stats.update();
+    }
 
     function render() {
-        stats.begin();
-
-        requestAnimationFrame(render);
         renderer.render(scene, camera);
-        stats.end();
     }
-    render();
 
+
+    $(window).on('resize', function (e) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        controls.handleResize();
+
+        render();
+    });
+
+
+    init();
+    createScene();
+    animate();
 
 });
